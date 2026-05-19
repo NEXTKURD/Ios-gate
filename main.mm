0798377117dcce402ee37aa744992ad7694eb975
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 23:07:55 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,15 +1,16 @@
 #import <UIKit/UIKit.h>
 #include "KeyAuth.h"
 
-// نیشاندانا لۆگینێ (هیچ گوهۆڕین د ڤێ بەشێ دا نینە)
+using namespace KeyAuth; // ئەڤە دێ ڕێگریێ ل خەتا "no member" کەت
+
 UIViewController *getTopViewController() {
     UIWindowScene *scene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
     return scene.windows.firstObject.rootViewController;
 }
 
 void showKeyInput() {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth Login"
-        message:@"تکایە کلیلێ بنڤێسە:"
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth"
+        message:@"کلیلێ بنڤێسە:"
         preferredStyle:UIAlertControllerStyleAlert];
 
     [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
@@ -19,11 +20,8 @@ void showKeyInput() {
     [alert addAction:[UIAlertAction actionWithTitle:@"لۆگین" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         NSString *userKey = alert.textFields[0].text;
         
-        // **ل ڤێرێ راستڤەکرن**: بکارئینانا `KeyAuth::` و `login`
-        KeyAuth::login([userKey UTF8String]);
-        
-        // **ل ڤێرێ راستڤەکرن**: بکارئینانا `KeyAuth::response.success`
-        if (KeyAuth::response.success) {
+        // **گوهۆڕینا گرنگ**: بەرهەمێ `login`ـێ راستەوخۆ وەرگرە
+        if (api.login([userKey UTF8String])) {
             NSLog(@"سەرکەفتن!");
         } else {
             showKeyInput();
@@ -33,9 +31,9 @@ void showKeyInput() {
     [getTopViewController() presentViewController:alert animated:YES completion:nil];
 }
 
-// **ل ڤێرێ راستڤەکرن**: بکارئینانا `KeyAuth::initApp`
 __attribute__((constructor)) static void init_gate() {
-    KeyAuth::initApp("NAVA_APP", "OWNER_ID", "SECRET");
+    // ئەگەر APIـێ تە وەک `api` هاتییە نڤێسین ل `KeyAuth.h`
+    api.initApp("NAVA_APP", "OWNER_ID", "SECRET");
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         showKeyInput();
