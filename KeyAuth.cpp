14eacf0177a04d7bde1f8af3dd0c7a8b74b1e7bf
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 22:28:15 +0300

    Update KeyAuth.cpp

--- KeyAuth.cpp
+++ KeyAuth.cpp
@@ -1,18 +1,7 @@
 #include "KeyAuth.h"
 #include <iostream>
 
-KeyAuth::Response KeyAuth::response = {false, ""};
-
 void KeyAuth::initApp(std::string name, std::string ownerid, std::string secret) {
-    // ئەڤە تەنێ بۆ ئەوە کو پڕۆژە پێ ب زانیت KeyAuth هەیە
-    std::cout << "App Initialized: " << name << std::endl;
-}
-
-void KeyAuth::login(std::string key) {
-    // ل ڤێرێ دێ گرێدان دگەل سێرڤەری هێتە ئەنجامدان
-    if(key == "test") {
-        response.success = true;
-    } else {
-        response.success = false;
-    }
+    // ل ڤێرێ کۆدێ تە دێ کار کەت
+    std::cout << "KeyAuth Initialized" << std::endl;
 }
