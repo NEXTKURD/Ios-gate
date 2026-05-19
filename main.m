7d83e1587ef7cae837f1344645000ea3780bdadb
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 22:05:42 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,42 +1,12 @@
 #import <UIKit/UIKit.h>
+#import "KeyAuth.h"
 
 __attribute__((constructor)) static void init_gate() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *window = nil;
-        if (@available(iOS 13.0, *)) {
-            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    window = scene.windows.firstObject;
-                    break;
-                }
-            }
-        }
-        if (!window) {
-            window = [[UIApplication sharedApplication] keyWindow];
-        }
-        
-        UIViewController *root = window.rootViewController;
-        if (!root) return;
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Security System"
-                                                                       message:@"Please enter the access password:"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        
-        [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-            textField.placeholder = @"Password";
-            textField.secureTextEntry = YES;
-        }];
-        
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"Login" style:UIAlertActionStyleDefault handler:^(UIAlertAction *act) {
-            UITextField *field = alert.textFields.firstObject;
-            if ([field.text isEqualToString:@"HOGR-UNLIMITED-77D37319A6B3"]) {
-                // Password is correct
-            } else {
-                exit(0);
-            }
-        }];
-        
-        [alert addAction:action];
-        [root presentViewController:alert animated:YES completion:nil];
-    });
+    // گرێدانا پڕۆژەیێ تە ب سێرڤەرێ KeyAuth
+    [KeyAuth initApp:@"Hogrbarwary05's Application" 
+              ownerID:@"ctPb2f2sjA" 
+              secret:@"ba700459d6d81c91c44465c45f2f85939f1a4a4297d198aa112372a0010a4c2f"];
+    
+    // ل ڤێرێ دێ پسیار ژ بکارئینەری کەی یان هەر کارەکێ تە ڤێت ئەنجام دەی
+    // ئەڤ کۆدە دێ بکارئینەری دگەل سێرڤەرێ KeyAuth گرێ دەت
 }
