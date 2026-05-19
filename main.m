66312c616a8ca43f49f444085a06129a3216fa16
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 20:07:29 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,27 +1,38 @@
 #import <UIKit/UIKit.h>
 
 __attribute__((constructor)) static void init_gate() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_to_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-        UIViewController *root = window.rootViewController;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    window = scene.windows.firstObject;
+                    break;
+                }
+            }
+        }
+        if (!window) {
+            window = [[UIApplication sharedApplication] keyWindow];
+        }
         
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"🔐 سیستەمێ پاراستنێ"
-                                                                       message:@"تکایە پاسۆردێ گشتی داخل بکە:"
+        UIViewController *root = window.rootViewController;
+        if (!root) return;
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Security System"
+                                                                       message:@"Please enter the access password:"
                                                                 preferredStyle:UIAlertControllerStyleAlert];
         
         [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-            textField.placeholder = @"پاسۆرد ل ڤێرە بنڤێسە";
+            textField.placeholder = @"Password";
             textField.secureTextEntry = YES;
         }];
         
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"چوونەژوورێ" style:UIAlertActionStyleDefault handler:^(UIAlertAction *act) {
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"Login" style:UIAlertActionStyleDefault handler:^(UIAlertAction *act) {
             UITextField *field = alert.textFields.firstObject;
-            
-            // پاسۆرد ل ڤێرە یێ دیارکرییە: 9999
             if ([field.text isEqualToString:@"9999"]) {
-                // پاسۆرد ڕاستە
+                // Password is correct
             } else {
-                exit(0); 
+                exit(0);
             }
         }];
         
