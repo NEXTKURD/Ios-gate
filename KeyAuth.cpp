59929167ded716433914c6cb5d5772228ee558e3
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 23:26:13 +0300

    Delete KeyAuth.cpp

--- KeyAuth.cpp
+++
@@ -1,7 +0,0 @@
-#include "KeyAuth.h"
-#include <iostream>
-
-void KeyAuth::initApp(std::string name, std::string ownerid, std::string secret) {
-    // ل ڤێرێ کۆدێ تە دێ کار کەت
-    std::cout << "KeyAuth Initialized" << std::endl;
-}
