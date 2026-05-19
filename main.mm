b717965fabc71e15156117f86e1bc33a1dfce914
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 23:10:50 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,7 +1,8 @@
 #import <UIKit/UIKit.h>
 #include "KeyAuth.h"
 
-using namespace KeyAuth; // ئەڤە دێ ڕێگریێ ل خەتا "no member" کەت
+// 1. دروستکرنا Objectـێ KeyAuth
+KeyAuth App("NAVA_APP", "OWNER_ID", "SECRET");
 
 UIViewController *getTopViewController() {
     UIWindowScene *scene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
@@ -9,7 +10,7 @@ UIViewController *getTopViewController() {
 }
 
 void showKeyInput() {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth"
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth Login"
         message:@"کلیلێ بنڤێسە:"
         preferredStyle:UIAlertControllerStyleAlert];
 
@@ -20,8 +21,8 @@ void showKeyInput() {
     [alert addAction:[UIAlertAction actionWithTitle:@"لۆگین" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         NSString *userKey = alert.textFields[0].text;
         
-        // **گوهۆڕینا گرنگ**: بەرهەمێ `login`ـێ راستەوخۆ وەرگرە
-        if (api.login([userKey UTF8String])) {
+        // 2. لۆگینکرن ب `App.login`
+        if (App.login([userKey UTF8String])) {
             NSLog(@"سەرکەفتن!");
         } else {
             showKeyInput();
@@ -32,8 +33,8 @@ void showKeyInput() {
 }
 
 __attribute__((constructor)) static void init_gate() {
-    // ئەگەر APIـێ تە وەک `api` هاتییە نڤێسین ل `KeyAuth.h`
-    api.initApp("NAVA_APP", "OWNER_ID", "SECRET");
+    // 3. ئینیتکرن (ئەگەر پێدڤی بیت، بەلێ زۆربەیا لایبراریان دگەل دروستکرنا `KeyAuth App` ئینیت دبن)
+    App.init();
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         showKeyInput();
