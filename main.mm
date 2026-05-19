336653e1dfb566452494e3d3b6d1929b2eb73928
NEXT STORE <hogrbarwary05@gmail.com>
2026-05-19 23:13:22 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,16 +1,9 @@
 #import <UIKit/UIKit.h>
 #include "KeyAuth.h"
 
-// 1. دروستکرنا Objectـێ KeyAuth
-KeyAuth App("NAVA_APP", "OWNER_ID", "SECRET");
-
-UIViewController *getTopViewController() {
-    UIWindowScene *scene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
-    return scene.windows.firstObject.rootViewController;
-}
-
+// 1. ئەڤە دێ لۆگینێ کەت (بێ دروستکرنا کلاسێ)
 void showKeyInput() {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth Login"
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KeyAuth"
         message:@"کلیلێ بنڤێسە:"
         preferredStyle:UIAlertControllerStyleAlert];
 
@@ -21,20 +14,21 @@ void showKeyInput() {
     [alert addAction:[UIAlertAction actionWithTitle:@"لۆگین" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         NSString *userKey = alert.textFields[0].text;
         
-        // 2. لۆگینکرن ب `App.login`
-        if (App.login([userKey UTF8String])) {
+        // **تاقیکرنا ڤی شێوازێ**: ل دەرڤەی کلاسێ
+        if (KeyAuth::login([userKey UTF8String])) {
             NSLog(@"سەرکەفتن!");
         } else {
             showKeyInput();
         }
     }]];
 
-    [getTopViewController() presentViewController:alert animated:YES completion:nil];
+    UIWindowScene *scene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
+    [scene.windows.firstObject.rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
 __attribute__((constructor)) static void init_gate() {
-    // 3. ئینیتکرن (ئەگەر پێدڤی بیت، بەلێ زۆربەیا لایبراریان دگەل دروستکرنا `KeyAuth App` ئینیت دبن)
-    App.init();
+    // 2. تاقیکرنا `init` ب ڤی شێوەی
+    KeyAuth::init("NAVA_APP", "OWNER_ID", "SECRET");
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         showKeyInput();
